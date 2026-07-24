8495bb8ebfc86141376cda031e4f9b962577b22f
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-24 13:19:56 +0530

    led blink

---
+++ 01_led_blink/src/main.cpp
@@ -0,0 +1,15 @@
+#define F_CPU 16000000UL
+#include<avr/io.h>
+#include<util/delay.h>
+int main()
+{
+
+  DDRB|=(1<<DDB3);
+  while(1)
+  {
+    PORTB|=(1<<PORTB3);
+    _delay_ms(1000);
+    PORTB&=~(1<<PORTB3);
+    _delay_ms(1000);
+  }
+}
